f7eed23f2324d3d29a18e17e6f2bf24d77f27274
Davut2005 <2005davut55@gmail.com>
2026-01-17 20:20:47 +0200

    Creating 7 tables for e-commerce

---
+++ create_tables.sql
@@ -0,0 +1,50 @@
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL UNIQUE,
+    price NUMERIC(10,2) NOT NULL CHECK(price > 0),
+    category_id INT REFERENCES categories( category_id )
+);   
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users( user_id ) ON DELETE CASCADE,
+    order_status VARCHAR(50),
+    order_date DATE DEFAULT CURRENT_DATE  
+);
+
+CREATE TABLE order_items (
+    order_id INT REFERENCES orders( order_id ) ON DELETE CASCADE,
+    product_id INT REFERENCES products( product_id ),
+    quantity INT NOT NULL CHECK(quantity > 0),
+    PRIMARY KEY( order_id, product_id )
+);
+
+CREATE TABLE payments (
+    payment_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id) ON DELETE CASCADE,
+    payment_date DATE DEFAULT CURRENT_DATE,
+    amount NUMERIC(10,2) NOT NULL CHECK(amount > 0),
+    payment_method VARCHAR(50)
+);
+
+CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users( user_id ) ON DELETE CASCADE,
+    product_id INT REFERENCES products( product_id ) ON DELETE CASCADE,
+    rating INT CHECK(rating BETWEEN 1 AND 5),
+    review TEXT,
+    review_date DATE DEFAULT CURRENT_DATE
+);
